e00c6b531f62623ffc4a8c5fcbdafae897ed99d6
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-08-07 17:09:36 -0500

    Initial commit created schema

---
+++ schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS clientes;
+
+CREATE DATABASE clientes;
+
+USE clientes;
+
+CREATE TABLE cliente(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nobre varchar(50) NOT NULL,
+  apellido_paterno varchar(50) NOT NULL,
+  apellido_materno varchar(50) NOT NULL
+);
+
+INSERT INTO() VALUES(),
