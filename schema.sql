7c5843da5370b1209ceb2a6b35f118cbc55d6bd4
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-08-07 17:11:48 -0500

    fixed minor bug misspelled nombre

--- schema.sql
+++ schema.sql
@@ -6,9 +6,9 @@ USE clientes;
 
 CREATE TABLE cliente(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nobre varchar(50) NOT NULL,
+  nombre varchar(50) NOT NULL,
   apellido_paterno varchar(50) NOT NULL,
   apellido_materno varchar(50) NOT NULL
 );
 
-INSERT INTO() VALUES(),
+INSERT INTO(nombre, apellido_paterno, apellido_materno) VALUES(Cesar, Tapia, Martinez),
